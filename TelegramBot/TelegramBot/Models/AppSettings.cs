67fce16dbf1bfc874b0e77057e1c6352ae64324c
VVendor <48651254+VVendor@users.noreply.github.com>
2019-12-23 23:03:16 +0300

    Update AppSettings.cs

--- TelegramBot/TelegramBot/Models/AppSettings.cs
+++ TelegramBot/TelegramBot/Models/AppSettings.cs
@@ -9,7 +9,7 @@ namespace TelegramBot.Models
     {
         public static string CURRENCYURL = "https://finance.tut.by/kurs/";
         public static string TABLECLASSNAME = "tbl-lite rates-table";
-        public static string BOTKEY = "1050319595:AAGMtiOCSGKMEnZN6vkzNzywaLVu81vyxyM";
+        public static string BOTKEY = "Api_key";
         public static string URL = "https://telegrambotapp.azurewebsites.net:443/{0}";
     }
 }
