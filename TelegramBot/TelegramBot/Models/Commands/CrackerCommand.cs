11cab1061bdcc9fa8ce8edbcde35add946deabd4
VVendor <48651254+VVendor@users.noreply.github.com>
2019-12-25 18:18:14 +0300

    Update CrackerCommand.cs

--- TelegramBot/TelegramBot/Models/Commands/CrackerCommand.cs
+++ TelegramBot/TelegramBot/Models/Commands/CrackerCommand.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 
@@ -11,7 +12,7 @@ namespace TelegramBot.Models.Commands
     {
         public override string Name => "/cracker";
 
-        public override async void Execute(Message message, TelegramBotClient botClient)
+        public override async Task Execute(Message message, TelegramBotClient botClient)
         {
             string answer = $"Привет, Утка! Кря-Кря)\n {"https://cdn.pixabay.com/photo/2018/03/10/08/35/duck-3213533_1280.jpg"}";
             await botClient.SendTextMessageAsync(message.Chat.Id, answer, disableWebPagePreview: false);
