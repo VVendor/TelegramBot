0a92a56b713beb2d06b8d867d0d86a807e50e2ed
VVendor <vadprof10@gmail.com>
2019-12-26 00:00:34 +0300

    add startCommand

---
+++ TelegramBot/TelegramBot/Models/Commands/StartCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBot.Models.Commands
+{
+    public class StartCommand : Command
+    {
+        public override string Name => "start";
+        public override async Task Execute(Message message, TelegramBotClient botClient)
+        {
+            await botClient.SendVideoAsync(message.Chat.Id, video:"https://frm-wows-ru.wgcdn.co/wows_forum_ru/monthly_2019_04/gif_1553969701.gif.369c1015d0645b002394455d63bde757.gif");
+        }
+    }
+}
