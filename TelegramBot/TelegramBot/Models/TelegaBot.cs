5b8527e74cf158393f806f1f447a9515ad0986de
VVendor <48651254+VVendor@users.noreply.github.com>
2019-12-25 18:15:20 +0300

    Update TelegaBot.cs

--- TelegramBot/TelegramBot/Models/TelegaBot.cs
+++ TelegramBot/TelegramBot/Models/TelegaBot.cs
@@ -1,38 +1,17 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web;
-using Telegram.Bot;
 using TelegramBot.Models.Commands;
 
 namespace TelegramBot.Models
 {
     public static class TelegaBot
     {
-        private static Telegram.Bot.TelegramBotClient botClient;
-        private static List<Command> commandsList;
-        public static IReadOnlyList<Command> Commands { get { return commandsList.AsReadOnly();} }
-
-        public static async Task<TelegramBotClient> Get()
+        public static List<Command> GetCommands()
         {
-            if(botClient != null)
-            {
-                return botClient;
-            }
-
-            //TODO: add more commands
-
-            commandsList = new List<Command>();
+            List<Command> commandsList = new List<Command>();
             commandsList.Add(new MainCommand());
             commandsList.Add(new AllCommand());
             commandsList.Add(new CrackerCommand());
-
-
-            botClient = new TelegramBotClient(AppSettings.BOTKEY);
-            var hook = string.Format(AppSettings.URL, "api/message/update");
-            await botClient.SetWebhookAsync(hook);
-            return botClient;
+            return commandsList;
         }
 
     }
